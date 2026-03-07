dcb9e4ce8cbdc607a66ce6d2592e036d51b41449
umHafsa <umHafsa@users.noreply.github.com>
2026-03-07 07:33:14 +0000

    deploy: 912a1918d84f43c4d792b7ee04d7a035fb9930a1 umHafsa/umHafsa-source@912a1918d84f43c4d792b7ee04d7a035fb9930a1

---
+++ _nuxt/2t5gO1tQ.js
@@ -0,0 +1,1 @@
+import{_ as o,o as s,c as r,G as t}from"./XPOGvugW.js";const n={};function c(e,a){return s(),r("tbody",null,[t(e.$slots,"default")])}const l=Object.assign(o(n,[["render",c]]),{__name:"ProseTbody"});export{l as default};
